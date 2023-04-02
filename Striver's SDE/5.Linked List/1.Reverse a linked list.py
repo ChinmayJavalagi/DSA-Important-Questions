ff9e782fcf796fd9fd2b9205e83436410e0ab888
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-04-02 11:26:53 +0530

    Reverse a linked list

--- Striver's SDE/5.Linked List/1.Reverse a linked list.py
+++ Striver's SDE/5.Linked List/1.Reverse a linked list.py
@@ -1,3 +1,11 @@
+"""
+1. Initialise prev --> None at first.
+2. Start traversing, store the cur.next in a variable
+3. Assign cur.next pointer to prev
+4. move prev to the next node.
+5. move cur to the next node using the variable that has stored cur.next
+"""
+
 #TC - O(n)
 class Solution(object):
     def reverseList(self, head):
