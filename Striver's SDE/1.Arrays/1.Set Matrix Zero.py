5bce0bee8ec3f836c7fdd287d0201d1f60c533d0
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-04-02 06:51:08 +0530

    Set Matrix Zero

--- Striver's SDE/1.Arrays/1.Set Matrix Zero.py
+++ Striver's SDE/1.Arrays/1.Set Matrix Zero.py
@@ -1,1 +1,28 @@
+'''Keep two lists to store i and j wherever we get a 0, 
+then traverse the matrix and check if either of i or j is present in first list or second, 
+if present set matrix[i][j] = 0'''
 
+# TC - O(N*M + N*M)
+# SC - O(N)
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        arr = []
+        arr1 = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    arr.append(i)
+                    arr1.append(j)
+                    
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if j in arr1 or i in arr:
+                    matrix[i][j] = 0
+
+
+        return matrix
