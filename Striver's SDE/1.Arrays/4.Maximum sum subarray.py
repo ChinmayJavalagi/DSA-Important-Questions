42064618fe9e3388a7928277dfb060cccc343c6a
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-04-02 07:13:35 +0530

    Kadane's Algorithm/Maximum sum subarray

--- Striver's SDE/1.Arrays/4.Maximum sum subarray.py
+++ Striver's SDE/1.Arrays/4.Maximum sum subarray.py
@@ -3,8 +3,8 @@
 """
 1. Initialize max_sum = -inf and temp_sum = 0
 2. Iterate through the array, add the val to temp_sum. 
-3. If temp_sum is negative reset it to 0 
-    elif temp_sum if greater than max_sum assign max_sum ---> temp_sum.  
+3. If temp_sum is negative reset it to 0 (becoz taking -ve values furthur isn't useful).
+    elif temp_sum is greater than max_sum assign max_sum ---> temp_sum.  
 """
 # TC - O(N)
 # SC - O(1)
