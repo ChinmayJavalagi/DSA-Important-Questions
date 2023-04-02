7ee30194f7cd03c14602f7a3dd761c8109ef5ce3
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-04-02 07:04:45 +0530

    3.Next Permutation

---
+++ Striver's SDE/1.Arrays/3.Next Permutation.py
@@ -0,0 +1,32 @@
+""" The next permutation of an array of integers is the next lexicographically greater permutation of its integer.
+    
+    Input: nums = [1,2,3]         Input: nums = [3,2,1]
+    Output: [1,3,2]               Output: [1,2,3]
+    
+    1.Traverse from back, look for num smaller than previous num (nums[i]<nums[i+1]), call it as "First".
+    2.Traverse from back till first, look for num just greater than "First", call it as "Second".
+    3.Swap "First" and "Second".
+    4.Reverse the list from First+1.
+    
+"""
+
+# TC - O(N)
+# SC - O(1)
+
+class Solution(object):
+    def nextPermutation(self, nums):
+
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                break
+        if i<0:
+            nums.reverse()
+        else:
+
+            for j in range(len(nums)-1,i-1,-1):
+                if nums[j]>nums[i]:
+                    break
+        
+            nums[i], nums[j] = nums[j], nums[i]
+
+            nums[i+1:] = nums[i+1:][::-1]
