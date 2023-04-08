461bd5c812db6a1d4aae5ef599943f33ae35388d
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-04-08 12:55:21 +0530

    Pattern searching

--- Striver's SDE/14.Strings/#.Pattern searching.py
+++ Striver's SDE/14.Strings/#.Pattern searching.py
@@ -60,3 +60,78 @@ def distpatsearch(txt, pat) :
         else :
             i += j 
    
+
+
+#Rabin-Karp Algorithm
+"""
+1. Initially calculate the hash value of the pattern.
+2. Start iterating from the starting of the string:
+        ->Calculate the hash value of the current substring having length m.
+        ->If the hash value of the current substring and the pattern are same check if the substring is same as the pattern(like naive).
+        ->If they are same, store the starting index as a valid answer (here we are just returning true). Otherwise, continue for the next substrings.
+3.Return the starting indices as the required answer(here true or false).
+"""
+
+"""
+***Spurious Hits***
+When the hash value of the pattern matches with the hash value of 
+a window of the text but the window is not the actual pattern then it is called a spurious hit.
+
+Spurious hit increases the time complexity of the algorithm. In order to minimize spurious hit,
+we use modulus. It greatly reduces the spurious hit.
+"""
+
+#TC - O((n-m+1)*m) ->Worst case (all spurious hits, "AAAAA", "AAA")
+   #- O(m + n) -> Average/Best case      
+
+class Solution:
+    def search(self,p,s):
+        # d is the number of characters in the input alphabet
+        d = 256
+        
+        def rksearch(pat,txt,q):
+            #q -> A prime number
+            m = len(pat)
+            n = len(txt)
+            h = 1
+            p = 0     # hash value for pattern
+            t = 0     # hash value for txt
+            
+            # The value of h would be "pow(d, M-1)%q"
+            for i in range(1,m):
+                h = (h*d)%q
+            
+            # Calculate the hash value of pattern and first window of text
+            for i in range(m):
+                p = (p*d + ord(pat[i]))%q
+                t = (t*d + ord(txt[i]))%q
+            
+            # Slide the pattern over text one by one
+            for i in range(n-m+1):
+                
+                ''' Check the hash values of current window of text and
+                pattern if the hash values match then only check 
+                for characters one by one.'''
+
+                if p == t:
+                    #Check for spurious hits.
+                    for j in range(m):
+                        if pat[j]!=txt[i+j]:
+                            break
+                    else:
+                        return True
+                    
+                # Calculate hash value for next window of text:
+                            #->Remove leading digit, add trailing digit
+                if i<n-m:
+                    t = (d*(t-ord(txt[i])*h) + (ord(txt[i+m])))%q
+                    
+                    # We might get negative values of t, converting it to positive
+                    if t<0:
+                        t+=q
+                        
+        return rksearch(p, s, 1009)
+    
+    
+    
+    
