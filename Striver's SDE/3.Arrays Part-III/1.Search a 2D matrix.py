5d0f4594e38c0f5cc7d351cb6b2f0aaa42ddb1fd
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-06-15 11:39:40 +0530

    Update 1.Search a 2D matrix.py

--- Striver's SDE/3.Arrays Part-III/1.Search a 2D matrix.py
+++ Striver's SDE/3.Arrays Part-III/1.Search a 2D matrix.py
@@ -23,3 +23,29 @@ class Solution:
             else:
                 h = m-1
         return False
+
+    
+#approach 2
+'''
+Streach the matrix by considering it as a linear array, if 3x4 matrix, we can say it as a linear array of length 12(3x4).
+Do binary search
+'''
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        low,high = 0,((m*n)-1)
+        while low<=high:
+            middle = (low+high)//2  
+            if matrix[middle//n][middle%n]>target:
+                high = middle-1
+            elif matrix[middle//n][middle%n]<target:
+                low = middle+1
+            else:
+                return True
+        return False
