8e78d9815c49369b98dfc3d363b148f4ddc6d5bc
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-06-17 06:35:46 +0530

    Update 1.Set Matrix Zero.py

--- Striver's SDE/1.Arrays/1.Set Matrix Zero.py
+++ Striver's SDE/1.Arrays/1.Set Matrix Zero.py
@@ -1,28 +1,43 @@
+#bruteforce
 '''Keep two lists to store i and j wherever we get a 0, 
 then traverse the matrix and check if either of i or j is present in first list or second, 
 if present set matrix[i][j] = 0'''
 
-# TC - O(N*M + N*M)
-# SC - O(N)
 
+#optimal
+# TC - O(N*M + N*M)
+# SC - O(1)
 class Solution(object):
     def setZeroes(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        arr = []
-        arr1 = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        r = len(matrix)
+        c = len(matrix[0])
+        col = 1
+        for i in range(r):
+            for j in range(c):
                 if matrix[i][j] == 0:
-                    arr.append(i)
-                    arr1.append(j)
-                    
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if j in arr1 or i in arr:
+                    if i == 0:
+                        col = 0
+                    else:
+                        matrix[0][j] = 0
+                        matrix[i][0] = 0
+        
+        print(matrix)
+        for i in range(1,r):
+            if matrix[i][0] == 0:
+                for j in range(1,c):
                     matrix[i][j] = 0
+        print(matrix)
 
+        for j in range(c):
+            if matrix[0][j] == 0:
+                for i in range(1,r):
+                    matrix[i][j] = 0
+        print(matrix)
 
-        return matrix
+        if col == 0:
+            for j in range(c):
+                matrix[0][j] = 0
