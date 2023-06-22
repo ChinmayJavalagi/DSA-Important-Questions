7f34a46440047bffa7354ae11f04380823581e42
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-06-22 07:01:59 +0530

    Update 2.Pascal's Triangle.py

--- Striver's SDE/1.Arrays/2.Pascal's Triangle.py
+++ Striver's SDE/1.Arrays/2.Pascal's Triangle.py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def generate(self, n):
+        li = [[1]*(i+1) for i in range(n)]
+        
+        for i in range(2,n):
+            for j in range(1,i):
+                li[i][j]=li[i-1][j-1]+li[i-1][j]
+                
+        return li
