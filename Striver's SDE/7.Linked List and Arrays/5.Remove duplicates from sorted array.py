bf4d1bdba477c8a5fa616c47022efacd54e444ea
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-07-23 09:16:51 +0530

    Update 5.Remove duplicates from sorted array.py

--- Striver's SDE/7.Linked List and Arrays/5.Remove duplicates from sorted array.py
+++ Striver's SDE/7.Linked List and Arrays/5.Remove duplicates from sorted array.py
@@ -7,6 +7,10 @@
 3. Return i+1
 """
 
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
 #Time complexity: O(n)
 class Solution(object):
     def removeDuplicates(self, nums):
