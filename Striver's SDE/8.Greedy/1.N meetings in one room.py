d25b8d7654569bd8a947d951e5ebda23e4853717
Chinmay Avinash Javalagi <67673355+ChinmayJavalagi@users.noreply.github.com>
2023-07-23 09:22:31 +0530

    Update 1.N meetings in one room.py

--- Striver's SDE/8.Greedy/1.N meetings in one room.py
+++ Striver's SDE/8.Greedy/1.N meetings in one room.py
@@ -8,6 +8,13 @@
 #TC - O(n) +O(n log n) + O(n) ~O(n log n)
 #SC - O(n)
 
+Input:
+N = 6
+start[] = {1,3,0,5,8,5}
+end[] =  {2,4,6,7,9,9}
+Output: 4
+Explanation: Maximum four meetings can be held with given start and end timings. The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
+
 class Solution:
     #Function to find the maximum number of meetings that can
     #be performed in a meeting room.
